e33c37aa7e8db70551531e615ea2519e920f3e48
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-12-07 22:50:18 -0500

    Kinda janky with some parameterized methods, but otherwise works

--- src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
@@ -7,23 +7,5 @@ public class MainApplication {
 
 
     public static void main(String[] args) {
-        SinglyLinkedList singlyLinkedList = new SinglyLinkedList(1);
-        System.out.println(singlyLinkedList.toString());
-        singlyLinkedList.add(2);
-        System.out.println(singlyLinkedList.toString());
-        singlyLinkedList.add(3);
-        System.out.println(singlyLinkedList.toString());
-        singlyLinkedList.add(4);
-        System.out.println(singlyLinkedList.toString());
-        System.out.println(singlyLinkedList.size());
-        singlyLinkedList.remove(2);
-        System.out.println(singlyLinkedList.toString());
-        System.out.println(singlyLinkedList.contains(5));
-        System.out.println(singlyLinkedList.find(2));
-        System.out.println(singlyLinkedList.get(2));
-        singlyLinkedList.add(2);
-        singlyLinkedList.add(6);
-        singlyLinkedList.add(3);
-        System.out.println(singlyLinkedList.sortASC(singlyLinkedList));
     }
 }
